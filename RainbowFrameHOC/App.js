f003e92fda31456f3274e299c4ccea57a62f2018
Les84Brest <lesmail@tut.by>
2020-10-07 07:49:07 +0300

    double button

--- RainbowFrameHOC/App.js
+++ RainbowFrameHOC/App.js
@@ -2,13 +2,24 @@
 
 import React, { Fragment } from 'react'; 
 import ReactDOM from 'react-dom';
+import DoubleButton from './components/DoubleButton';
 import withRainbowFrame from './components/withRainbowFrame';
 
 let colors = ['red', 'OrangeRed', 'yellow', 'green', 'tomato'];
 colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
 let BorderCoomponent = withRainbowFrame(colors)(Fragment);
 
+
+
 ReactDOM.render(
-  <BorderCoomponent>Hello</BorderCoomponent>
+
+  // <BorderCoomponent>Hello</BorderCoomponent>
+  <DoubleButton cbPressed={toCons}  caption1="Тот, кто хочет стать пилотом," caption2="Потому что только смелый сам полезет в самолет" > Очень смелый видно тот!</DoubleButton>
+  
   , document.querySelector('.rainbow')
 );
+
+
+function toCons(num){
+  console.log(num);
+}
---
+++ RainbowFrameHOC/components/DoubleButton.js
@@ -0,0 +1,32 @@
+﻿import React, { Children, Component, Fragment } from "react";
+import PropTypes from 'prop-types';
+
+
+
+const DoubleButton = (props /*children, cbPressed, caption1, caption2,*/) => {
+  console.log(props);
+  let btn1Ref = React.createRef();
+  let btn2Ref = React.createRef();
+  const onClickAction = (e) => {
+    props.cbPressed(200);
+  }
+  
+  return (
+    <div className='double__button'>
+      <input type="button" ref={btn1Ref} value={props.caption1} onClick={onClickAction}/>
+      {props.children}
+      <input type="button" ref={btn2Ref} value={props.caption2} onClick={onClickAction}/>
+    </div>
+  );
+
+
+}
+
+DoubleButton.propTypes = {
+  children: PropTypes.node,
+  cbPressed: PropTypes.func,
+  caption1: PropTypes.string,
+  caption2: PropTypes.string,
+}
+
+export default DoubleButton;
