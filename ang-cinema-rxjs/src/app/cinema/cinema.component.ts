065373aaa69065edad69f9e75ee7c34b9ab43399
Les84Brest <lesmail@tut.by>
2021-04-23 22:36:44 +0300

    cinema 2 begin

---
+++ ang-cinema-rxjs/src/app/cinema/cinema.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TicketsEventsService } from '../tickets-event.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'cinema',
+  templateUrl: 'cinema.component.html',
+  styleUrls: ['cinema.component.css']
+})
+export class Cinema {
+  
+  constructor(){
+    
+  }
+
+}
